4113dba7fae4d6b187292013f13b29720caf4e44
Barbara Paes <barbara.paes@indicium.tech>
2024-10-29 11:28:08 +0000

    Foram criadas as stanging country,creditcard, personcreditcard e salesdetail

---
+++ models/Staging/erp/stg_erp__creditcard.sql
@@ -0,0 +1,11 @@
+with
+    creditcard as(
+        select
+            creditcardid
+            , cardtype 
+            , modifieddate
+        from{{ source('erp_adventureworks','CREDITCARD')}}
+    )
+
+select *
+from creditcard
---
+++ models/Staging/erp/stg_erp__personcreditcard.sql
@@ -0,0 +1,10 @@
+with
+    person_credit_card as(
+        select
+            creditcardid
+            , businessentityid
+        from{{ source('erp_adventureworks','PERSONCREDITCARD')}}
+    )
+
+select *
+from person_credit_card
