b2cb1185f8208bd11baf399769a5d53bdc98838a
Barbara Paes <barbara.paes@indicium.tech>
2024-10-30 19:24:27 +0000

    foi criado agora de maneira certa o dim_creditcard

--- models/Sales/Marts/dim_creditcard.sql
+++ models/Sales/Marts/dim_creditcard.sql
@@ -1,19 +1,61 @@
-version: 2
+{{ config(materialized='table') }}
 
-models:
-  - name: dim_creditcard
-    columns:
-      - name: sk_creditcard
-        data_tests:  
-          - unique
-          - not_null
-      - name: creditcardid
-        data_tests:  
-          - unique
-          - not_null        
-      - name: businessentityid
-        data_tests:  
-          - unique
-      - name: cardtype
-        data_tests:  
-          - not_null 
+with
+    creditcard as (
+        select
+            creditcardid,
+            cardtype,
+            modifieddate
+        from {{ ref('stg_erp__creditcard') }}
+    ),
+
+    person_credit_card as (
+        select
+            creditcardid,
+            businessentityid
+        from {{ ref('stg_erp__personcreditcard') }}
+    ),
+
+    person as (
+        select
+            businessentityid,
+            cast(firstname || ' ' || middlename || ' ' || lastname as varchar) as full_name
+        from {{ ref('stg_erp__person') }}
+    ),
+
+    joined_data as (
+        select
+            cc.creditcardid,
+            cc.cardtype,
+            cc.modifieddate as creditcard_modifieddate,
+            pcc.businessentityid,
+            p.full_name
+        from creditcard cc
+        join person_credit_card pcc on cc.creditcardid = pcc.creditcardid
+        join person p on pcc.businessentityid = p.businessentityid
+    ),
+
+    dim_creditcard as (
+        select 
+            {{ dbt_utils.generate_surrogate_key(['creditcardid', 'businessentityid']) }} as sk_creditcard,
+            *
+        from joined_data
+    )
+
+select *
+from dim_creditcard
+
+
+
+
+
+
+
+
+
+
+
+
+
+select *
+from creditcard
